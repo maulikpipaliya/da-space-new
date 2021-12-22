0973cebdc5b030bbb16142feb9789278d7e9fc77
Sachin Rathod <sachinrathod@Sachins-MacBook.local>
2021-12-22 23:41:40 +0530

    create placement api

--- backend/src/api/controllers/placement.controller.js
+++ backend/src/api/controllers/placement.controller.js
@@ -1,14 +1,7 @@
 import asyncHandler from "express-async-handler"
 import placementService from "../services/placement.service.js"
 
-// export const helloPlacement = asyncHandler(async (req, res) => {
-//     res.status(200).send({
-//         success: true,
-//         message: "Welcome to Placement APIs",
-//     })
-// })
-
-export const getAllPlacementRecords = asyncHandler(async (req, res) => {
+export const getAllPlacementRecords = async (req, res) => {
     console.log("Getting all placement records")
     try {
         const placements = await placementService().getAllPlacementRecords()
@@ -16,9 +9,9 @@ export const getAllPlacementRecords = asyncHandler(async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message })
     }
-})
+}
 
-export const addPlacement = asyncHandler(async (req, res) => {
+export const addPlacement = async (req, res) => {
     const placement = req.body
     try {
         const newPlacement = await new placementService().addPlacement(
@@ -33,4 +26,42 @@ export const addPlacement = asyncHandler(async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message })
     }
-})
+}
+export const getPlacementById = async (req, res) => {
+    const id = req.params.id
+    try {
+        const placement = await placementService().getPlacementById(id)
+        res.status(200).json(placement)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
+}
+
+export const updatePlacement = async (req, res) => {
+    const id = req.params.id
+    const placement = req.body
+    try {
+        const updatedPlacement = await placementService().updatePlacement(
+            id,
+            placement.companyName,
+            placement.jobProfile,
+            placement.yearOfDrive,
+            placement.roundNameX,
+            placement.roundNameY,
+            placement.roundNameXDescriptioconst
+        )
+        res.status(200).json(updatedPlacement)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
+}
+
+export const deletePlacement = async (req, res) => {
+    const id = req.params.id
+    try {
+        const deletedPlacement = await placementService().deletePlacement(id)
+        res.status(200).json(deletedPlacement)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
+}
--- backend/src/api/routes/placement.router.js
+++ backend/src/api/routes/placement.router.js
@@ -1,11 +1,18 @@
 import { Router } from "express"
 
-import { addPlacement } from "../controllers/placement.controller.js"
+import { addPlacement,
+    updatePlacement,
+    deletePlacement,
+    getPlacementById,
+    getAllPlacementRecords } from "../controllers/placement.controller.js"
 
 const router = Router({
     mergeParams: true,
 })
 
 router.post("/addPlacement", addPlacement)
-
+router.get("/getPlacementById:placementID", getPlacementById)
+router.get("/getAllPlacementRecords", getAllPlacementRecords)
+router.put("/updatePlacement", updatePlacement)
+router.delete("/deletePlacement:placementID", deletePlacement)
 export default router
