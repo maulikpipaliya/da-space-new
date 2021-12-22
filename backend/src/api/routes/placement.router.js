8b8fe813c94729e81f79a1163c9deb4ffc817a90
Maulik Pipaliya <maulik.pipaliya@gmail.com>
2021-12-22 15:16:13 +0530

    Error Fix

--- backend/src/api/routes/placement.router.js
+++ backend/src/api/routes/placement.router.js
@@ -1,15 +1,11 @@
 import { Router } from "express"
 
-import {
-    helloPlacement,
-    addPlacement,
-} from "../controllers/placement.controller.js"
+import { addPlacement } from "../controllers/placement.controller.js"
 
 const router = Router({
     mergeParams: true,
 })
 
-router.get("/", helloPlacement)
 router.post("/addPlacement", addPlacement)
 
 export default router
